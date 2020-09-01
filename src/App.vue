696f70aaa27cc2aeb2f921a4afe02a502d426861
Ercan Uzunsakal <ercan112358@gmail.com>
2020-09-01 20:54:56 +0300

    vuetify and vue-class-component installed.

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,12 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <router-view></router-view>
+  </v-app>
 </template>
 
-<style lang="scss">
-  @import "../node_modules/ag-grid-community/dist/styles/ag-grid.css";
-  @import "../node_modules/ag-grid-community/dist/styles/ag-theme-alpine.css";
-</style>
+<script>
+export default {
+  name: "App",
+  data: () => ({}),
+};
+</script>
