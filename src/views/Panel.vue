c1537e2411d1a325b41b46c0bade9ee6c6b1eedf
Ercan Uzunsakal <ercan112358@gmail.com>
2020-09-02 11:12:05 +0300

    Create Panel.vue

---
+++ src/views/Panel.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <general-table></general-table>
+  </div>
+</template>
+<script>
+import GeneralTable from "@/components/GeneralTable";
+export default {
+  components: { GeneralTable },
+};
+</script>
