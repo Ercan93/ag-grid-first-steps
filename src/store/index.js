c9f46005f88c96ba7cb5b8d6c3aed4994a2477ec
Ercan Uzunsakal <ercan112358@gmail.com>
2020-09-04 22:05:35 +0300

    setDataCount actions function prepared.

--- src/store/index.js
+++ src/store/index.js
@@ -7,6 +7,8 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     gameData: veri,
+    publisherGameCount: null,
+    yearGamesCount: null,
   },
   getters: {
     gameDataGetters(state) {
@@ -14,6 +16,23 @@ export default new Vuex.Store({
     },
   },
   mutations: {},
-  actions: {},
+  actions: {
+    setDataCount(context, title) {
+      let gameData = context.state.gameData;
+      let dataCountList = new Map();
+      let count = 0;
+
+      gameData.forEach((element) => {
+        if (dataCountList.get(element[title]) == undefined) {
+          dataCountList.set(element[title], 1);
+        } else {
+          count = dataCountList.get(element[title]);
+          dataCountList.set(element[title], ++count);
+        }
+      });
+      title = title.toUpperCase();
+      context.commit(`SET_${title}_GAMES_COUNT`, dataCountList);
+    },
+  },
   modules: {},
 });
